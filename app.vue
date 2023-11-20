90dc76c27a0ac6bfecb0cbb06a5999d72ea3227b
Aspitrine <loquente62@gmail.com>
2023-11-20 09:58:26 +0100

    Utilisation du dossier `pages` pour gérer les routes + exemple de variable reactiver lier à un input

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-screen bg-amber-500">
+    <header class="bg-amber-600 text-white font-bold p-2 text-center">
+      <h1 class="text-3xl hover:bg-[#F0F]">Pokedex</h1>
+    </header>
+    <main>
+      <NuxtPage />
+    </main>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="text-center m-2">
+      Recherche :
+      <!--  le v-model permet de lier la donnée à mon input  -->
+      <input v-model="searchText" type="text" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Le ref remplace le useState
+// Cela me permet de créer une variable réactive
+// Ici je n'ai pas une fonction pour modifier ma variable
+const searchText = ref("");
+</script>
